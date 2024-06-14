e9891296217c1ee2f41d92feac418be328936e63
Mukesh-Kumar-Balusami <balusamimukesh@gmail.com>
2024-06-14 16:43:19 +0530

    V002

---
+++ script.js
@@ -0,0 +1,6 @@
+function togglemenu(){
+    const menu = document.querySelector(".burger-icon");
+    menu.classList.toggle("open")
+    const icon = document.querySelector(".burger-links");
+    icon.classList.toggle("open")
+}
